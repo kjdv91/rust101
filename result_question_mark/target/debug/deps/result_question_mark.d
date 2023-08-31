7fb1cadf944223278d3aacb4a494e121f3b43be8
kjdv91 <126703713+kjaramillo91@users.noreply.github.com>
2023-08-31 10:07:13 -0700

    results_question_mark

---
+++ result_question_mark/target/debug/deps/result_question_mark.d
@@ -0,0 +1,5 @@
+E:\PracticasProgrmación\practicas-rust\result_question_mark\target\debug\deps\result_question_mark.exe: src\main.rs
+
+E:\PracticasProgrmación\practicas-rust\result_question_mark\target\debug\deps\result_question_mark.d: src\main.rs
+
+src\main.rs:
---
+++ result_question_mark/target/debug/result_question_mark.d
@@ -0,0 +1,1 @@
+E:\PracticasProgrmación\practicas-rust\result_question_mark\target\debug\result_question_mark.exe: E:\PracticasProgrmación\practicas-rust\result_question_mark\src\main.rs
